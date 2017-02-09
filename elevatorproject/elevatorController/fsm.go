9726b3e775e52788791908dc1afc56a1dae4fce8
erlingrj <erling.jellum@gmail.com>
2017-02-09 13:41:45 +0100

    Add files via upload

---
+++ elevatorproject/elevatorController/fsm.go
@@ -0,0 +1,60 @@
+package elevatorController
+
+import (
+	."./../driver"
+	"fmt"
+	"time"
+
+)
+
+func GoToFloor(floor int){
+	//Takes the elevator to _floor_, opens the door for 3 secs and closes it. Returns 1 on success
+
+	
+
+	if (GetFloorSensorSignal() == -1) {
+		SetMotorDirection(DirnUp)
+
+			for (GetFloorSensorSignal() == -1) {}
+			SetMotorDirection(DirnStop)
+	}
+
+
+
+	
+	if (floor - GetFloorSensorSignal()) > 0 {
+		SetMotorDirection(DirnUp) 
+	} else if (floor - GetFloorSensorSignal()) < 0 {
+		SetMotorDirection(DirnDown)
+	}
+
+	fmt.Println(GetMotorDirection())
+	for (GetFloorSensorSignal() != floor) {
+	}
+
+
+
+
+	SetMotorDirection(DirnStop)
+	OpenDoors()
+
+
+
+
+
+
+	//else if (floor == GetFloorSensorSignal()) 
+
+
+}
+
+
+func OpenDoors() {
+	if GetMotorDirection() != DirnStop {
+		fmt.Println("gjør noe")
+	}
+	SetDoorOpenLamp(1)
+	time.Sleep(3*time.Second)
+	SetDoorOpenLamp(0)
+}
+
---
+++ elevatorproject/elevatorController/runElevator.go
@@ -0,0 +1,32 @@
+package elevatorController
+
+/*import (
+	."./../driver"
+	"fmt"
+)
+
+func RunElevator() {
+
+    //Reading sensors and buttons. 
+
+    for {
+
+        if GetButtonSignal(ButtonCallUp, 0)  {
+
+        	orderButtonPressed(5)
+        }
+    }
+
+
+
+
+}
+
+
+func orderButtonPressed(int floor , ButtonType button, chan  [] byte) {
+
+	//Sets the button light and sends order to OrderModule
+
+}
+
+*/
