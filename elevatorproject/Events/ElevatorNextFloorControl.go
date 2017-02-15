4bf0862156a9abe371545239599123de0f1e7cb5
Audun <audunhem@gmail.com>
2017-02-15 11:58:48 +0100

    Add files via upload

---
+++ elevatorproject/Events/ElevatorNextFloorControl.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "fmt"
+  "sort"
+)
+
+const THIS_ELEVATOR = 1
+const N_ELEVATORS = 3
+const N_FLOORS = 4
+const (
+	DirnDown = -1 + iota
+	DirnStop
+	DirnUp
+)
+
+type Elevator struct{
+  InternalOrders []int
+  ExternalOrders []int //trenger noe for å vise retningen til ordren
+  Direction int
+  CurrentFloor int
+  ID int
+}
+
+func RecieveNewState(a,b,c,d chan int){
+  select{
+  case c <- x:
+    //sett noe til noe
+  case c2 <- y:
+    //sett noe annet til noe annet
+  }
+}
